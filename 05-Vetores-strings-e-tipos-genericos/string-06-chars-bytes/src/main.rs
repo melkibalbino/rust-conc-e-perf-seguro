ad5015a2262252de97f44302428d6cb783f666c6
melkibalbino <melkibalbinoads@gmail.com>
2021-08-02 10:44:33 -0300

    interact a string with chars, bytes, and enumerate

---
+++ 05-Vetores-strings-e-tipos-genericos/string-06-chars-bytes/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let name = String::from("rust");
+
+    for chr in name.chars() {
+        println!("Char: {}", chr);
+    }
+
+    println!("--------------------------");
+
+    for bt in name.bytes() {
+        println!("Byte: {}", bt);
+    }
+
+    println!("--------------------------");
+
+    // iterar com indice
+    for (idx, chr) in name.chars().enumerate() {
+        println!("Indice: {}, Char: {}", idx, chr);
+    }
+}
