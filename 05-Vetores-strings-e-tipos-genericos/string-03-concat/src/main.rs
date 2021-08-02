cc02a8dbf867346950d96928452ec1bca4e34e52
melkibalbino <melkibalbinoads@gmail.com>
2021-08-02 00:40:47 -0300

    use push and push_str to concatenate

--- 05-Vetores-strings-e-tipos-genericos/string-03-concat/src/main.rs
+++ 05-Vetores-strings-e-tipos-genericos/string-03-concat/src/main.rs
@@ -11,5 +11,26 @@ fn main() {
     setence += &autor;
     setence += ".";
 
-    println!("{}", setence)
+    println!("{}", setence);
+
+    let mut setence2 = String::from("Este é o livro ");
+    setence2.push_str(&livro);
+    setence2.push_str(" da ");
+    setence2.push_str(&editora);
+    setence2.push_str(" e escrito por ");
+    setence2.push_str(&autor);
+    setence2.push_str(".");
+
+    println!("-----------------------");
+    println!("{}",setence2);
+
+    // Usando Char para Contatenar
+    let mut sentence3 = String::from("Hey ");
+    sentence3.push('Y');
+    sentence3.push('a');
+    sentence3.push('a');
+    sentence3.push('h');
+    sentence3.push('!');
+    println!("-----------------------");
+    println!("{}", sentence3)
 }
